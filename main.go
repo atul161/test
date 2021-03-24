d0002e219c88f91384ea40f31f53a1fbe2bf7844
atul161 <atulanand199898dbg@gmail.com>
2021-03-24 06:07:26 +0530

    added

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	log.Print("Hello how are you")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello hi, %q", html.EscapeString(r.URL.Path))
 	})
